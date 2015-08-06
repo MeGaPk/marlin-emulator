8c952c5f33970a5030fde3b1d6222cf5459207b9
Ivan <ivan@itbsg.eu>
2015-08-06 18:54:39 +0300

    Second version

--- marlin-emulator/marlin-emulator.ino
+++ marlin-emulator/marlin-emulator.ino
@@ -1,55 +1,65 @@
 /*
- Author: MeGaPk (Ivan)
+  author: Ivan Gaydamakin
  */
 
-char inputData[50];
-char len=0;
+char inputString[256];
+char len = 0;
 
 
 void setup() {
   // initialize serial:
-  Serial.begin(115200);
+  Serial.begin(250000);
 }
+const int extruder_temperature = 21;
+const int bed_temperature = 20;
 
 void loop() {
 }
 
 void serialEvent() {
   while (Serial.available()) {
-    char inChar = (char)Serial.read(); 
+    char inChar = (char)Serial.read();
+    inputString[len++] = inChar;
 
     if (inChar == '\n') {
-      inputData[len]=0;
+      inputString[len++] = 0;
       process_command();
-      len = 0;
-    }
-    else if(len<sizeof(inputData)-1){
-      inputData[len++] = inChar;
-    }
-    else{
       len = 0;
     }
   }
 }
 
-void process_command(){
-  Serial.println("ok");
+void process_command() {
+  //  Serial.print("|");
+  //  Serial.print(inputString);
+  //  Serial.println("|");
+  if (strstr (inputString, "M") != NULL) {
+    execute_m_gcode();
+  }
+  else
+  {
+    Serial.println("ok");
+  }
 }
 
 
+void execute_m_gcode() {
+  if (strstr (inputString, "M105") != NULL) {
+    //ok T:35.2 /0.0 B:0.0 /0.0 T0:35.2 /0.0 @:0 B@:0
+    Serial.print("ok T:");
+    Serial.print(extruder_temperature);
+    Serial.print(" /0.0");
+    Serial.print(" B:");
+    Serial.print(bed_temperature);
+    Serial.print(" /0.0");
+    Serial.print(" T0:");
+    Serial.print(extruder_temperature);
+    Serial.print(" /0.0");
+    Serial.print(" @:0 B@:0");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Serial.println();
+  }
+  else {
+    Serial.println("ok");
+  }
+}
